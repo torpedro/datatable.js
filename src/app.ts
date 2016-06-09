e01a50666fe25194ff267c72a0a4998a49ce3b34
Pedro <pedroflemming@gmail.com>
2016-06-10 01:38:21 +0200

    reduce public interface to common classes

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,14 @@ import * as smooth from './smooth/__module__';
 import * as io from './io/__module__';
 import * as table from './table/__module__';
 
+import { Vector, Set, HashMap } from './data/__module__';
+import { AnalyticsTable as Table } from './table/__module__';
+import { CSVParser } from './io/__module__';
+
 export {
-	data,
-	smooth,
-	io,
-	table
+	Vector,
+	Set,
+	HashMap,
+	Table,
+	CSVParser
 };
