2fa6a3967e78a49398cc82bdb980e84070301ae9
Pedro Flemming <pedroflemming@gmail.com>
2017-11-12 19:54:21 +0000

    update definition file

---
+++ example/example.ts
@@ -0,0 +1,9 @@
+/// <reference path="../dist/datatable.js.d.ts" />
+import { Table } from "datatable.js";
+
+let table = new Table({
+  fields: ['One'],
+  types: ['number']
+});
+
+table.insert([[1]])
